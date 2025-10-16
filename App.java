8004896e6ad7af430f04c641c54cd4764b60d825
ErikaLaine <erika.laine@student.hamk.fi>
2025-10-16 11:09:21 +0300

    Create App.java

---
+++ App.java
@@ -0,0 +1,17 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hei olen Tulostin-ohjelma");
+        
+        String tekijä = "Erika Laine";
+        System.out.println("Ohjelman tekijä: " + tekijä);
+
+        
+        double luku1 = 5;
+        double luku2 = 2;
+
+        System.out.println("Luku1-muuttujan arvo on" + luku1);
+        System.out.println("Luku2-muuttujan arvo on" + luku2);
+    
+        
+    }
+}
